bdbcebfcbc40312f1f777df3456b2ff27e687f2b
MrMiles94 <aniefiokasuquo33@gmail.com>
2022-06-09 08:05:50 +0100

    index on halfway

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
-window.addEventListener('load',re=>{document.getElementById('window').style.height = `${window.innerHeight}px`;})
-window.addEventListener('resize',re=>{document.getElementById('window').style.height = `${window.innerHeight}px`;})
+window.addEventListener('load',()=>{document.getElementById('window').style.height = `${window.innerHeight -50}px`;})
+window.addEventListener('resize',()=>{document.getElementById('window').style.height = `${window.innerHeight - 50}px`;})
+
+document.getElementById('search-icon').addEventListener('click', 
+    ()=>{
+        document.getElementById('search-form').style.display = 'flex'
+        document.getElementById('search-icon').style.display = 'none'
+    }
+)
